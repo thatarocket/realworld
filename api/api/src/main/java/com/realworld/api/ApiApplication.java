bf16d8734657e1591a8a88ca9e9c79726702dc91
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-12-27 23:30:26 -0300

    add Springboot

---
+++ api/api/src/main/java/com/realworld/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package com.realworld.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	// @GetMapping("/")
+	// public String hello() {
+	// 	return "Hello World";
+	// }
+
+
+}
+ 
