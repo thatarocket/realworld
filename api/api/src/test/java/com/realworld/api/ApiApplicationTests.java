c71df6b37c2795aa6e9fe902dcf1cd1fe1c71f30
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-12-28 23:47:14 -0300

    add junit

--- api/api/src/test/java/com/realworld/api/ApiApplicationTests.java
+++ api/api/src/test/java/com/realworld/api/ApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.realworld.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a = 1;
+        int b = 1;
+        assertEquals(a, b); // just for test if it is working
 	}
 
 }
